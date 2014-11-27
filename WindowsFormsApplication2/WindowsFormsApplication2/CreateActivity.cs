dd917157f3aae612217c08d5695628ae94bf022f
Simon Sondrup Kristensen <simonsondrup@gmail.com>
2014-11-27 12:25:47 +0100

    GUI til opret projekt

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/CreateActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class CreateActivity : Form
+    {
+        public CreateActivity()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveActivity(object sender, EventArgs e)
+        {
+
+            string title = textTitle.Text;
+            string tags = textTag.Text;
+            int spots = 0;
+
+            if (checkIfNumber(textSpots.Text))
+            { 
+                spots = Convert.ToInt32(textSpots.Text);
+            }
+            string lector = textLector.Text;
+            string address = textAddress.Text;
+            string city = textCity.Text;
+            DateTime date = dateTimePicker.Value;
+        }
+
+        private bool checkIfNumber(string txt) //Check to see if string only contains digits
+        {
+            bool pass = true;
+            foreach (var ch in txt)
+            {
+                if (!char.IsDigit(ch))
+                {
+                    pass = false;
+                }
+            }
+            return pass;
+        }
+
+        
+
+    }
+
+    
+}
