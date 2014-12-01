c0b7891991e37d9349b0a2e8bd9878188db39514
Marck Jensen <marckjensen93@hotmail.com>
2014-12-01 12:28:39 +0100

    Rettet opret aktivitet GUI

--- WindowsFormsApplication2/WindowsFormsApplication2/CreateActivity.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/CreateActivity.Designer.cs
@@ -31,28 +31,19 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.textTitle = new System.Windows.Forms.TextBox();
             this.textTag = new System.Windows.Forms.TextBox();
-            this.textSpots = new System.Windows.Forms.TextBox();
-            this.textLector = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.richTextBoxDescription = new System.Windows.Forms.RichTextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textAddress = new System.Windows.Forms.TextBox();
-            this.textCity = new System.Windows.Forms.TextBox();
-            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(581, 433);
+            this.button1.Location = new System.Drawing.Point(775, 533);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Opret";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,156 +52,71 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(20, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.Size = new System.Drawing.Size(39, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Titel:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Location = new System.Drawing.Point(20, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.Size = new System.Drawing.Size(44, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tags:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 65);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Pladser:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 90);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Afholder:";
-            // 
             // textTitle
             // 
-            this.textTitle.Location = new System.Drawing.Point(83, 12);
+            this.textTitle.Location = new System.Drawing.Point(115, 39);
+            this.textTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textTitle.Name = "textTitle";
-            this.textTitle.Size = new System.Drawing.Size(100, 20);
+            this.textTitle.Size = new System.Drawing.Size(132, 22);
             this.textTitle.TabIndex = 7;
             // 
             // textTag
             // 
-            this.textTag.Location = new System.Drawing.Point(83, 37);
+            this.textTag.Location = new System.Drawing.Point(115, 70);
+            this.textTag.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textTag.Name = "textTag";
-            this.textTag.Size = new System.Drawing.Size(100, 20);
+            this.textTag.Size = new System.Drawing.Size(132, 22);
             this.textTag.TabIndex = 8;
-            // 
-            // textSpots
-            // 
-            this.textSpots.Location = new System.Drawing.Point(83, 62);
-            this.textSpots.Name = "textSpots";
-            this.textSpots.Size = new System.Drawing.Size(100, 20);
-            this.textSpots.TabIndex = 9;
-            this.textSpots.Text = "0";
-            // 
-            // textLector
-            // 
-            this.textLector.Location = new System.Drawing.Point(83, 87);
-            this.textLector.Name = "textLector";
-            this.textLector.Size = new System.Drawing.Size(100, 20);
-            this.textLector.TabIndex = 10;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 123);
+            this.label5.Location = new System.Drawing.Point(16, 151);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.Size = new System.Drawing.Size(84, 17);
             this.label5.TabIndex = 11;
             this.label5.Text = "Beskrivelse:";
             // 
             // richTextBoxDescription
             // 
-            this.richTextBoxDescription.Location = new System.Drawing.Point(15, 139);
+            this.richTextBoxDescription.Location = new System.Drawing.Point(20, 171);
+            this.richTextBoxDescription.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBoxDescription.Name = "richTextBoxDescription";
-            this.richTextBoxDescription.Size = new System.Drawing.Size(641, 288);
+            this.richTextBoxDescription.Size = new System.Drawing.Size(853, 354);
             this.richTextBoxDescription.TabIndex = 12;
             this.richTextBoxDescription.Text = "";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(265, 15);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(48, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Adresse:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(265, 40);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(22, 13);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "By:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(265, 65);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(57, 13);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Tidspunkt:";
-            // 
-            // textAddress
-            // 
-            this.textAddress.Location = new System.Drawing.Point(345, 12);
-            this.textAddress.Name = "textAddress";
-            this.textAddress.Size = new System.Drawing.Size(100, 20);
-            this.textAddress.TabIndex = 17;
-            // 
-            // textCity
-            // 
-            this.textCity.Location = new System.Drawing.Point(345, 37);
-            this.textCity.Name = "textCity";
-            this.textCity.Size = new System.Drawing.Size(100, 20);
-            this.textCity.TabIndex = 18;
-            // 
-            // dateTimePicker
-            // 
-            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePicker.Location = new System.Drawing.Point(345, 62);
-            this.dateTimePicker.Name = "dateTimePicker";
-            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker.TabIndex = 21;
-            // 
             // CreateActivity
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 468);
-            this.Controls.Add(this.dateTimePicker);
-            this.Controls.Add(this.textCity);
-            this.Controls.Add(this.textAddress);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(891, 576);
             this.Controls.Add(this.richTextBoxDescription);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textLector);
-            this.Controls.Add(this.textSpots);
             this.Controls.Add(this.textTag);
             this.Controls.Add(this.textTitle);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "CreateActivity";
             this.Text = "Opret aktivitet";
             this.ResumeLayout(false);
@@ -223,20 +129,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textTitle;
         private System.Windows.Forms.TextBox textTag;
-        private System.Windows.Forms.TextBox textSpots;
-        private System.Windows.Forms.TextBox textLector;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.RichTextBox richTextBoxDescription;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textAddress;
-        private System.Windows.Forms.TextBox textCity;
-        private System.Windows.Forms.DateTimePicker dateTimePicker;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
