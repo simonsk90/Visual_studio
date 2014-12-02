f1fc4abdb507decb2c4a7428d006406bfe75709a
Nicolai <Nicolai887@gmail.com>
2014-12-02 10:39:57 +0100

    Updateret testklasse

--- WebApi/UnitTestProject/DBConnectionTest.cs
+++ WebApi/UnitTestProject/DBConnectionTest.cs
@@ -31,5 +31,14 @@ namespace UnitTestProject
             Activity a = dba.getActivityByID(2);
             Console.WriteLine(a.title);
         }
+
+
+        public void TestGetEvent()
+        {
+            DBEvent dbe = new DBEvent();
+            dbe.addEvent(new Event(new DateTime(), new Location("Hey", "fagvej", 8900, "YoBy"), "Marcks mor", new Activity(89,"Neeeej", "Blaaa","fucker,popcorn,#OgMinMaaaajd")));
+            Event e = dbe.getEventByID(0);
+            Console.WriteLine(e.lecturer + " lecturer og " + e.date + " date");
+        }
     }
 }
